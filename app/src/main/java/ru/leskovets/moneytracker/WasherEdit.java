820f1c0773440e489f7fa38efea49c0d3e81e093
Nexpi <yuril@list.ru>
2017-06-28 16:48:24 +0300

    rev. 1.0.4 - add class Washer for two: name&price;

--- app/src/main/java/ru/leskovets/moneytracker/WasherEdit.java
+++ app/src/main/java/ru/leskovets/moneytracker/WasherEdit.java
@@ -6,36 +6,32 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
-/**
- * Created by yuril on 27.06.2017.
- */
-
 public class WasherEdit {
 
 
-//    final TextView add = (TextView) findViewById(R.id.add);
-//    final EditText name = (EditText) findViewById(R.id.name);
-//    final EditText number = (EditText) findViewById(R.id.price);
-//
-//    TextWatcher watcher = new TextWatcher() {
-//        @Override
-//        public void beforeTextChanged(CharSequence s, int start, int before, int count) {
-//
-//        }
-//
-//        @Override
-//        public void onTextChanged(CharSequence s, int start, int before, int count) {
-//            add.setEnabled(!TextUtils.isEmpty(name.getText().toString().trim()) && !TextUtils.isEmpty(price.getText().toString().trim()));
-//        }
-//
-//        @Override
-//        public void afterTextChanged(Editable s) {
-//
-//        }
-//    };
-//
-//        name.addTextChangedListener(watcher);
-//        number.addTextChangedListener(watcher);
+    final TextView add = (TextView) findViewById(R.id.add);
+    final EditText name = (EditText) findViewById(R.id.name);
+    final EditText price = (EditText) findViewById(R.id.price);
+
+    TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            add.setEnabled(!TextUtils.isEmpty(name.getText().toString().trim()) && !TextUtils.isEmpty(price.getText().toString().trim()));
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+        name.addTextChangedListener(watcher);
+        price.addTextChangedListener(watcher);
 
 }
 
