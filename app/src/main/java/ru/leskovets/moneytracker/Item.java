1f8ef2b50bdb7ddaebcea18a2f4546e52ab101f4
Nexpi <yuril@list.ru>
2017-07-06 15:29:44 +0300

    rev. 1.0.7 - add RecyclerView;

--- app/src/main/java/ru/leskovets/moneytracker/Item.java
+++ app/src/main/java/ru/leskovets/moneytracker/Item.java
@@ -5,13 +5,13 @@ package ru.leskovets.moneytracker;
  */
 
     public class Item {
-//        final String name;
-//        final int price;
-//
-//        public Item(String name, int price) {
-//            this.name = name;
-//            this.price = price;
-//        }
+        final String name;
+        final int price;
+
+        public Item(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
 
 
     }
--- app/src/main/java/ru/leskovets/moneytracker/MainActivity.java
+++ app/src/main/java/ru/leskovets/moneytracker/MainActivity.java
@@ -24,83 +24,84 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        add = (TextView) findViewById(R.id.add);
-        name = (EditText) findViewById(R.id.name);
-        number = (EditText) findViewById(R.id.price);
-       //final RecyclerView items = (RecyclerView) findViewById(R.id.items);
-        //items.setAdapter(new ItemsAdapter());
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.items);
+        //add = (TextView) findViewById(R.id.add);
+        //name = (EditText) findViewById(R.id.name);
+        //number = (EditText) findViewById(R.id.price);
+       final RecyclerView items = (RecyclerView) findViewById(R.id.items);
+        items.setAdapter(new ItemsAdapter());
 
 
-        TextWatcher watcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                add.setEnabled(!TextUtils.isEmpty(name.getText().toString().trim()) && !TextUtils.isEmpty(number.getText().toString().trim()));
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-
-        };
-
-        name.addTextChangedListener(watcher);
-        number.addTextChangedListener(watcher);
+//        TextWatcher watcher = new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                add.setEnabled(!TextUtils.isEmpty(name.getText().toString().trim()) && !TextUtils.isEmpty(number.getText().toString().trim()));
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//
+//        };
+//
+//        name.addTextChangedListener(watcher);
+//        number.addTextChangedListener(watcher);
     }
 
-//    private class ItemsAdapter extends RecyclerView.Adapter<ItemViewHolder> {
-//        final List<Item> items = new ArrayList<>();
-//
-//        ItemsAdapter() {
-//            items.add(new Item("milk Prostokvashino 3,2%", 60));
-//            items.add(new Item("apple", 50));
-//            items.add(new Item("fun maker", 100));
-//            items.add(new Item("lemon 1 kg", 40));
-//            items.add(new Item("fruits 3 kg", 1000));
-//            items.add(new Item("red fish", 670));
-//            items.add(new Item("bread", 50));
-//            items.add(new Item("rice 1 sht.", 33));
-//            items.add(new Item("icecream Korovka", 58));
-//            items.add(new Item("gun", 4550));
-//            items.add(new Item("car Lada Granda, red color", 100000));
-//            items.add(new Item("juice", 50));
-//            items.add(new Item("mouse", 750));
-//            items.add(new Item("android super smartfon Lenovo K5 Vibe", 4550));
-//        }
-//
-//        @Override
-//        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//            return new ItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, null));
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(ItemViewHolder holder, int position) {
-//            final Item item = items.get(position);
-//            holder.name.setText(item.name);
-//            holder.price.setText(String.valueOf(item.price));
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return items.size();
-//        }
-//    }
-//
-//    private class ItemViewHolder extends RecyclerView.ViewHolder {
-//        private final TextView name, price;
-//
-//        ItemViewHolder(View itemView) {
-//            super(itemView);
-//            name = (TextView) itemView.findViewById(R.id.name);
-//            price = (TextView) itemView.findViewById(R.id.price);
-//        }
-//    }
+    private class ItemsAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+        final List<Item> items = new ArrayList<>();
+
+        ItemsAdapter() {
+            items.add(new Item("milk Prostokvashino 3,2%", 60));
+            items.add(new Item("apple", 50));
+            items.add(new Item("fun maker", 100));
+            items.add(new Item("lemon 1 kg", 40));
+            items.add(new Item("fruits 3 kg", 1000));
+            items.add(new Item("red fish", 670));
+            items.add(new Item("bread", 50));
+            items.add(new Item("rice 1 sht.", 33));
+            items.add(new Item("icecream Korovka", 58));
+            items.add(new Item("gun", 4550));
+            items.add(new Item("car Lada Granda, red color", 100000));
+            items.add(new Item("juice", 50));
+            items.add(new Item("mouse", 750));
+            items.add(new Item("android super smartfon Lenovo K5 Vibe", 4550));
+        }
+
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, null));
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            final Item item = items.get(position);
+            holder.name.setText(item.name);
+            holder.price.setText(String.valueOf(item.price));
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+    }
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView name, price;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.name);
+            price = (TextView) itemView.findViewById(R.id.price);
+        }
+    }
 
     @Override
     protected void onResume() {
