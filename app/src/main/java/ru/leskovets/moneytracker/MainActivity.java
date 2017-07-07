e27b2b796550d977ce85b2c987c5be2dd50abf47
Nexpi <yuril@list.ru>
2017-07-07 11:39:25 +0300

    rev. 1.0.4.1 - add Fragments;

--- app/src/main/java/ru/leskovets/moneytracker/MainActivity.java
+++ app/src/main/java/ru/leskovets/moneytracker/MainActivity.java
@@ -1,5 +1,10 @@
 package ru.leskovets.moneytracker;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -17,6 +22,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
     TextView add;
     EditText name;
     EditText number;
@@ -24,13 +30,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.items);
+        setContentView(R.layout.activity_main);
+        final TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        final ViewPager pages = (ViewPager) findViewById(R.id.pages);
+        //setContentView(R.layout.items);
         //add = (TextView) findViewById(R.id.add);
         //name = (EditText) findViewById(R.id.name);
         //number = (EditText) findViewById(R.id.price);
-       final RecyclerView items = (RecyclerView) findViewById(R.id.items);
-        items.setAdapter(new ItemsAdapter());
+        //final RecyclerView items = (RecyclerView) findViewById(R.id.items);
+        //items.setAdapter(new ItemsAdapter());
+        pages.setAdapter(new MainPagerAdapter());
+        tabs.setupWithViewPager(pages);
 
 
 //        TextWatcher watcher = new TextWatcher() {
@@ -55,53 +65,7 @@ public class MainActivity extends AppCompatActivity {
 //        number.addTextChangedListener(watcher);
     }
 
-    private class ItemsAdapter extends RecyclerView.Adapter<ItemViewHolder> {
-        final List<Item> items = new ArrayList<>();
-
-        ItemsAdapter() {
-            items.add(new Item("milk Prostokvashino 3,2%", 60));
-            items.add(new Item("apple", 50));
-            items.add(new Item("fun maker", 100));
-            items.add(new Item("lemon 1 kg", 40));
-            items.add(new Item("fruits 3 kg", 1000));
-            items.add(new Item("red fish", 670));
-            items.add(new Item("bread", 50));
-            items.add(new Item("rice 1 sht.", 33));
-            items.add(new Item("icecream Korovka", 58));
-            items.add(new Item("gun", 4550));
-            items.add(new Item("car Lada Granda, red color", 100000));
-            items.add(new Item("juice", 50));
-            items.add(new Item("mouse", 750));
-            items.add(new Item("android super smartfon Lenovo K5 Vibe", 4550));
-        }
-
-        @Override
-        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, null));
-        }
-
-        @Override
-        public void onBindViewHolder(ItemViewHolder holder, int position) {
-            final Item item = items.get(position);
-            holder.name.setText(item.name);
-            holder.price.setText(String.valueOf(item.price));
-        }
-
-        @Override
-        public int getItemCount() {
-            return items.size();
-        }
-    }
-
-    private class ItemViewHolder extends RecyclerView.ViewHolder {
-        private final TextView name, price;
-
-        ItemViewHolder(View itemView) {
-            super(itemView);
-            name = (TextView) itemView.findViewById(R.id.name);
-            price = (TextView) itemView.findViewById(R.id.price);
-        }
-    }
+//
 
     @Override
     protected void onResume() {
@@ -111,5 +75,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    private class MainPagerAdapter extends FragmentPagerAdapter {
+        private final String[] titles;
+
+        MainPagerAdapter() {
+            super(getSupportFragmentManager());
+            titles = getResources().getStringArray(R.array.main_pager_titles);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            final ItemsFragment fragment = new ItemsFragment();
+            Bundle args = new Bundle();
+            args.putString(ItemsFragment.ARG_TYPE, Item.TYPE_EXPENSE);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
     }
 }
